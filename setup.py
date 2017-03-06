7ec4b9c180d17738fe6ac3345d98c78c831ff8ab
Sergio Garcez <garcez.sergio@gmail.com>
2017-03-06 22:15:11 +0000

    setup.py keywords

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     name='arseparse',
     version='0.1',
     description='Command-line app helper library',
-    author="Sergio Garcez",
+    author='Sergio Garcez',
     url='https://github.com/sgarcez/arseparse',
     py_modules=['arseparse'],
     setup_requires=['pytest-runner'],
     tests_require=['mock', 'pytest'],
+    keywords=['cli', 'argparse', 'command-line'],
 )
