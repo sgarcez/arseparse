70ecef12b06bf38eda2270fcd559a51655d728f6
Sergio Garcez <garcez.sergio@gmail.com>
2017-03-07 17:41:37 +0000

    More pypi metadata

--- setup.py
+++ setup.py
@@ -11,5 +11,18 @@ setup(
     py_modules=['arseparse'],
     setup_requires=['pytest-runner'],
     tests_require=['mock', 'pytest-cov', 'pytest'],
+    license='MIT',
     keywords=['cli', 'argparse', 'command-line'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
